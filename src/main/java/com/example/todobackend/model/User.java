ab8583e2a7d8c9797c287c8da94ab8f46e02531c
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-31 14:53:53 -0400

    Create User model

---
+++ src/main/java/com/example/todobackend/model/User.java
@@ -0,0 +1,9 @@
+package com.example.todobackend.model;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String password;
+}
