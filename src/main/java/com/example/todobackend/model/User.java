0a24b442dbdcd217f4e7711d4d7a8e67e6a684be
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-31 15:00:54 -0400

    Add User annotations

--- src/main/java/com/example/todobackend/model/User.java
+++ src/main/java/com/example/todobackend/model/User.java
@@ -1,9 +1,24 @@
 package com.example.todobackend.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column
     private String firstName;
+
+    @Column
     private String lastName;
+
+    @Column
     private String emailAddress;
+
+    @Column
     private String password;
 }
