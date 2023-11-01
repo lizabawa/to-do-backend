4fd38b62eab68fe2a7594d5ec251ea6264674062
Elizabeth Yang <elizabethyang928@gmail.com>
2023-11-01 13:41:11 -0400

    add to JWTUtils class and add JWT dependencies

--- src/main/java/com/example/todobackend/security/JWTUtils.java
+++ src/main/java/com/example/todobackend/security/JWTUtils.java
@@ -1,8 +1,22 @@
 package com.example.todobackend.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JWTUtils {
+    @Value("${jwt-secret}")
+    private String jwtSecret;
 
+    @Value("${jwt-expiration-ms}")
+    private int jwtExpirationMS;
+
+    public String generateJwtToken(MyUserDetails myUserDetails){
+        return Jwts.builder()
+                .setSubject((myUserDetails.getUsername())) //get just the email address
+                .setIssuedAt(new Date()) //set time to whatever the current time is
+                .setExpiration(new Date(new Date().getTime() + jwtExpirationMS)) //sets expiration date to current time + the jwt expiration date
+                .signWith(SignatureAlgorithm.HS256, jwtSecret) //HS256 is the algorithm
+                .compact(); //compacts everything together
+    }
 }
