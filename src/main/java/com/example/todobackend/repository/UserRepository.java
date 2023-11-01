512494dfcfcb1af1cab301a468ea7cddfc34a663
Elizabeth Yang <elizabethyang928@gmail.com>
2023-11-01 11:17:33 -0400

    Create UserRepository and security package

---
+++ src/main/java/com/example/todobackend/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.example.todobackend.repository;
+
+public interface UserRepository {
+}
--- src/main/java/com/example/todobackend/service/UserService.java
+++ src/main/java/com/example/todobackend/service/UserService.java
@@ -1,4 +1,7 @@
 package com.example.todobackend.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
 }
