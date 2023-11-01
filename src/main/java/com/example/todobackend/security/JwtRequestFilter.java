b8dd0deb6c704753288db2b9930728f25aef6c80
Elizabeth Yang <elizabethyang928@gmail.com>
2023-11-01 13:35:50 -0400

    Create JWTUtils class

--- src/main/java/com/example/todobackend/security/JwtRequestFilter.java
+++ src/main/java/com/example/todobackend/security/JwtRequestFilter.java
@@ -1,4 +1,8 @@
 package com.example.todobackend.security;
 
-public class JwtRequestFilter {
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
 }
--- src/main/java/com/example/todobackend/security/SecurityConfiguration.java
+++ src/main/java/com/example/todobackend/security/SecurityConfiguration.java
@@ -3,6 +3,8 @@ package com.example.todobackend.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -19,7 +21,7 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public JwtRequestFilter authenticationJwtTokenFilter() {
+    public JwtRequestFilter authJwtRequestFilter(){
         return new JwtRequestFilter();
     }
 
